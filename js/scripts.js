ba8a112e80fb48e1a61b1b901bed9286737659a7
BrendanRusso <russobrendanm@gmail.com>
2019-01-06 22:45:41 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+//Nav Highlighting
+var workNav=document.getElementById("Work"),infoNav=document.getElementById("Info"),infoPage=document.getElementById("content-info"),workPage=document.getElementById("content-work");infoPage?infoNav.style.fontWeight="bold":workPage&&(workNav.style.fontWeight="bold");
+//Nav Highlighting
---
+++ source/js/scripts.js
@@ -0,0 +1,24 @@
+//Nav Highlighting
+
+var workNav = document.getElementById('Work');
+
+var infoNav = document.getElementById('Info');
+
+var infoPage = document.getElementById("content-info");
+var workPage = document.getElementById("content-work");
+
+
+if(infoPage){
+
+    infoNav.style.fontWeight = 'bold';
+    // infoNav.classList.add('outline-text');
+    
+} else if (workPage){
+    workNav.style.fontWeight = 'bold';
+    // workNav.classList.add('outline-text');
+    
+}
+
+//Nav Highlighting
+
+
